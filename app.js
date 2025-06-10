9daa257892eb54a4ecf18d0801381a7624f5bc59
Anushka <anushkayadav80234@gmail.com>
2025-06-11 00:09:28 +0530

    Fix: use process.env.SECRET in session options

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ store.on("error", () => {
 
 const sessionOptions = {
     store,
-    secret: SECRET,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
